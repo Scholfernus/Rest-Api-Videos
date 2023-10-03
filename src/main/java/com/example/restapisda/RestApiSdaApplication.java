8ee1d544578a4fae02e822a2c75d639e99a1e54c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-03 22:10:37 +0200

    Rest Api main

---
+++ src/main/java/com/example/restapisda/RestApiSdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapisda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiSdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restapisda/RestApiSdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapisda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
